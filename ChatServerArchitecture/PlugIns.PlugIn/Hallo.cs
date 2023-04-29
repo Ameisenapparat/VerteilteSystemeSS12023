8ae43d180edb32a1a537d731543ecd213cd60e1b
Felix Voß <felix.voss2@stud.hs-flensburg.de>
2023-04-29 17:08:22 +0200

    Revert "remove duplicates"
    This reverts commit ea4b1f8f2fca45cad93e19da34ea64a98ec221a1.

---
+++ ChatServerArchitecture/PlugIns.PlugIn/Hallo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlugIns.PlugIn
+{
+    public class Hallo : PlugIns.Contract.MyPlugin
+    {
+        public string Do(string s)
+        {
+            return $"Hallo {s}!";
+        }
+    }
+}
